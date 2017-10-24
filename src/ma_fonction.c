f926d0dbc5e8885bc08f161c1c79c5678b4b2da1
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-10-24 12:51:47 -0400

    Modification de notre code pour utiliser la librairie

--- src/ma_fonction.c
+++ src/ma_fonction.c
@@ -1,10 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <autre_fonction_lib.h>
 
 int ma_fonction(void)
 {
 	printf("ma_fonction() called\n");
+	autre_fonction_lib();
 	printf("ma_fonction() returning\n");
 	return 0;
 }
--- src/main.c
+++ src/main.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 #include "ma_fonction.h"
+#include <fonction_lib.h>
 
 int main(void)
 {
 	printf("main() called\n");
 	ma_fonction();
+	fonction_lib();
 	printf("main() returning\n");
 	return 0;
 }
